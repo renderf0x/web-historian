3fb946107efa0c7016a3c8f86460de1175ae9375
renderf0x <derek@barncard.com>
2014-11-19 15:59:33 -0800

    finished archive helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var httpGet = require('http-request');
 var _ = require('underscore');
 
 /*
@@ -25,17 +26,73 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(callback){
+  fs.readFile(exports.paths.list, 'utf8', function(err, data) {
+    if(err) throw err;
+    callback(data.split('\n'));
+  });
 };
 
-exports.isUrlInList = function(){
+// exports.readListOfUrls(console.log);
+
+exports.isUrlInList = function(url, successCB, failCB){
+  successCB = successCB || _.identity;
+  failCB = failCB || _.identity;
+  exports.readListOfUrls(function(urlList) {
+    if(_.contains(urlList, url)){
+      successCB(urlList, url);
+    } else {
+      console.log('URL not in list');
+      failCB(urlList, url);
+    }
+  });
 };
 
-exports.addUrlToList = function(){
+// exports.isUrlInList('www.google.com',console.log);
+// exports.isUrlInList('www.google1.com', console.log);
+
+exports.addUrlToList = function(url){
+  exports.isUrlInList(url, function(){}, function(urlList, url){
+    fs.appendFile(exports.paths.list, url+'\n');
+  });
 };
 
-exports.isURLArchived = function(){
+exports.addUrlToList('www.urmama.com');
+
+exports.isURLArchived = function(url, successCB, failCB){
+  //console.log(path.join(exports.paths.archivedSites, url));
+  fs.exists(path.join(exports.paths.archivedSites, url), function(exists){
+    if (exists){
+      // console.log('exists!');
+      successCB(url);
+    } else {
+      // console.log('does not exist!')
+      failCB(url);
+    }
+  })
+};
+
+//exports.isURLArchived('www.google.com', console.log, function(){});
+
+exports.downloadUrl = function(url) {
+  httpGet.get(url, path.join(exports.paths.archivedSites, url), function (err, res) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(res.code, res.headers, res.file);
+  });
 };
 
 exports.downloadUrls = function(){
+  exports.readListOfUrls(function(listOfUrls){
+    // console.log(listOfUrls);
+    for (var i = 0; i < listOfUrls.length; i++){
+      exports.isURLArchived(listOfUrls[i], function(){}, exports.downloadUrl);
+    }
+  });
 };
+
+
+//exports.downloadUrl('www.yahoo.com');
+exports.downloadUrls();
