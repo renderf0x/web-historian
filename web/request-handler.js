efc2b3320088692bb32902d8934b789e74865661
renderf0x <derek@barncard.com>
2014-11-19 17:05:08 -0800

    added basic routing

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,16 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //need to grab the user submitted url
+
+  if(req.url === '/'){
+    httpHelpers.serveAssets(res, './public/index.html', function() {})
+  } else {
+    //should later refactor to modify header if it is a css file
+    httpHelpers.serveAssets(res, path.join(archive.paths.siteAssets,req.url), function() {})
+  }
+  //res.end(archive.paths.list);
 };
